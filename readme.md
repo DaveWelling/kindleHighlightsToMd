a9620580838745bb09b077ca9d02977028296808
david.welling <david.welling@sstid.com>
2024-12-29 10:10:00 -0500

    improve comments and readme

--- readme.md
+++ readme.md
@@ -1,22 +1,43 @@
+# Extract your Kindle highlights and notes
+
 The bookmarklet will extract the highlights and notes from the most recent book (or whatever book is displayed) in
 https://read.amazon.com/notebook and give you a json file with them.
 
-To use the bookmarklet,
+The `organizeIntoMd.js` code will turn the extracted json into a markdown file. You will probably want to fiddle with it to get the layout how you would prefer based on the colors of your highlights.
 
-```
-node ./tools/minify.js
-```
+## Highlight expectations
 
-will create ./dist/bookmarklet.min.js.
+You should probably change organizeIntoMd to set whatever expectations you want. Currently it assumes that:
+
+-   Headers
+    -   Chapters are Orange highlights and in the form: `/^\d/)` (e.g. _1 Some Chapter Title_)
+    -   Sub-chapters are Orange highlights and **not** in the form for Chapters or Principles (see below)
+    -   Principles are Orange highlights and in the form: `/^[a-zA-Z]{1,2}\d:/` (e.g. _Pr1 Some Principle Idea_)
+-   Content (inside Headers)
+    -   Examples are Blue highlights
+    -   Important things are Pink highlights
+    -   Regular highlights are Yellow highlights
+
+## To use the bookmarklet,
 
 Create a bookmark in chrome. Call it whatever you want, but paste the contents of ./dist/bookmarklet.min.js. into the url.
 
 Open https://read.amazon.com/notebook and click the bookmark. It will give you the json.
 
-Update HIGHLIGHTS_JSON_PATH constant in ./src/organizeIntoMd.js with the path of the json you just received.
+Update HIGHLIGHTS_JSON_PATH constant in ./src/organizeIntoMd.js with the path of the json you just received. Then run:
 
 ```
 node ./src/organizeIntoMd.js
 ```
 
 This will create a markdown file ./dist/organized_highlights.md
+
+## If you need to make a change to the bookmarklet
+
+```
+node ./tools/minify.js
+```
+
+will recreate ./dist/bookmarklet.min.js.
+
+You'll need to edit your Chrome bookmark and paste the changed bookmarklet.min.js content into the url.
--- src/organizeIntoMd.js
+++ src/organizeIntoMd.js
@@ -5,23 +5,24 @@ const HIGHLIGHTS_JSON_PATH =
     'C:\\Users\\david\\Downloads\\The_Principles_of_Product_Development_Flow__Second_Generation_Lean_Product_Development_highlights.json';
 
 try {
-    // read json from this file C:\Users\david\Downloads\Untitled_Book_highlights.json
+    // read json from this file C:\Users\me\Downloads\Some_Book_highlights.json
     const json = JSON.parse(fs.readFileSync(HIGHLIGHTS_JSON_PATH, 'utf8'));
     const highlights = json.highlights;
-    const asin = json.asin ?? 'B00K7OWG7O';
+    const asin = json.asin;
 
     let markdown = '';
-    // create a json hierarchy of the highlights using color
+    // create a hierarchy of the highlights using color
+    // Headers:
     // level 0: color === "Orange" highlights starting with a number are the top level chapters
     // level 2: color === "Orange" highlights starting with one or two letters, a number and a colon are principles
     // level 1: all other color === "Orange" highlights are subchapters containing principles
-    // level 3 array contains:
+    // Content:
     // color === "Yellow" highlights are regular
     // color === "Blue" highlights are examples
     // color === "Pink" highlights are important
-    // example result: {"1 some chapter": {"some subchapter": { "pp1 some principle": { highlights: [{type: "regular", text: "some text", location: "123", note: "some note"},{type: "example", text: "some example", location: "456", note: "some example note"},{type: "important", text: "some important text", location: "789", note: "some important note"}]}}}}
     let currentChapter, currentSubchapter, currentPrinciple;
     highlights.forEach((highlight) => {
+        // Headers of various types are Orange highlights
         if (highlight.color === 'Orange') {
             if (highlight.text.match(/^\d/)) {
                 currentChapter = highlight.text;
@@ -37,10 +38,14 @@ try {
                 markdown += `## ${highlight.text}\n`;
             }
         } else {
+            // Everything else is content and goes underneath the headers
             let highlightPath = [currentChapter, currentSubchapter, currentPrinciple, 'highlights'];
             // remove undefined values
             highlightPath = highlightPath.filter((value) => value !== undefined);
+            // depth is not really used now, but might be useful to others using the coe
             const depth = highlightPath.length;
+
+            // Format the content based on the color of the highlight
             if (highlight.color === 'Blue') {
                 markdown += `> [${highlight.text ?? highlight.note}](kindle://book/?action=open&asin=${asin}&location=${
                     highlight.location
@@ -66,9 +71,7 @@ try {
         }
     });
 
-    // Convert the JSON to a Markdown file
-
-    // Save results to a JSON file in the ../dist folder
+    // Save results to a markdown file in the ../dist folder
     console.log('Writing the file');
     const outputPath = path.join(__dirname, '..', 'dist', 'organized_highlights.md');
     fs.writeFileSync(outputPath, markdown);
